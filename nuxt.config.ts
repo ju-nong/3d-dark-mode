bac68b98ab4037b370bb7271c6cf332992bb9364
이준용 <iiiuooyoo@gmail.com>
2023-03-22 15:08:17 +0900

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ["@nuxtjs/tailwindcss"],
+
+    app: {
+        head: {
+            charset: "utf-8",
+            title: "3D Dark Mode",
+            meta: [
+                { name: "description", content: "다크모드를 3D로 만나보세요!" },
+                { name: "author", content: "ju-nong" },
+            ],
+        },
+    },
 });
