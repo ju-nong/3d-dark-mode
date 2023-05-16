9c6549ed242e0dffeebdbf8e3521b3c43d95ba4f
이준용 <iiiuooyoo@gmail.com>
2023-05-16 11:41:05 +0900

    refact : Meta 태그 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,8 +8,22 @@ export default defineNuxtConfig({
             title: "3D Dark Mode",
             meta: [
                 { name: "description", content: "다크모드를 3D로 만나보세요!" },
-                { name: "author", content: "ju-nong" },
+                { name: "author", content: "이준용" },
+                { name: "theme-color", content: "#000" },
+                { name: "keywords", content: "Darkmode, 3D, Cute" },
+                { property: "og:title", content: "3D Dark Mode" },
+                { property: "og:type", content: "website" },
+                {
+                    property: "og:site:name",
+                    content: "3d-dark-mode.vercel.app",
+                },
+                {
+                    property: "og:url",
+                    content: "https://3d-dark-mode.vercel.app/",
+                },
+                { property: "og:image", content: "/favicon.png" },
             ],
+            link: [{ rel: "icon", href: "/favicon.png", type: "image/png" }],
         },
     },
 });
